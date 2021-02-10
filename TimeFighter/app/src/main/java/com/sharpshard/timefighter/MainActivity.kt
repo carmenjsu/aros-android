3aee1a0255eadb0ef90d4397f040771898ebf32e
CSu <CSu@cardlytics.com>
2021-02-10 12:33:29 +0000

    waiting for verification from Google

--- TimeFighter/app/src/main/java/com/sharpshard/timefighter/MainActivity.kt
+++ TimeFighter/app/src/main/java/com/sharpshard/timefighter/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() { //class - inheritance
 
@@ -62,7 +63,13 @@ class MainActivity : AppCompatActivity() { //class - inheritance
     }
 
     private fun showinfo() {
+        val dialogTitle = getString(R.string.aboutTitle, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.aboutMessage)
 
+        val builder = AlertDialog.Builder(this) // this: ref current activity
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
     }
 
     private fun resetGame() {
